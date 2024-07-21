c3b4dc720b371e23e25b322002ee266053b0df58
Adithya Deepthi Kumar <114129263+Adithyadk@users.noreply.github.com>
2024-07-21 22:11:50 +0530

    Update and rename Lab4_AlphaBeta.ipynb to 4.AlphaBeta.py

---
+++ 4.AlphaBeta.py
@@ -0,0 +1,43 @@
+def alpha_beta(depth, nodeIndex, maximizingPlayer, values, alpha, beta, path):
+    if depth == 3:
+        return values[nodeIndex], path + [nodeIndex]
+
+    if maximizingPlayer:
+        best = float('-inf')
+        best_path = []
+        for i in range(2):
+            val, new_path = alpha_beta(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta, path + [nodeIndex])
+            if val > best:
+                best = val
+                best_path = new_path
+            
+            alpha=max(alpha,best)
+            if alpha>=beta:
+              print(f"Pruning:{path+[nodeIndex]}")
+              break
+            
+        return best, best_path
+
+    else:
+        best = float('inf')
+        best_path = []
+        for i in range(2):
+            val, new_path = alpha_beta(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta, path + [nodeIndex])
+            if val < best:
+                best = val
+                best_path = new_path
+            
+            beta=min(beta,best)
+            if alpha>=beta:
+              print(f"Pruning:{path+[nodeIndex]}")
+              break
+
+        return best, best_path
+
+# Example tree with depth 3 and 8 terminal nodes
+values = [3, 5, 2, 9, 12, 5, 23, 23]
+
+# Start the Min-Max algorithm
+optimal_value, optimal_path = alpha_beta(0, 0, True, values, float('-inf'), float('inf'), [])
+print("The optimal value is:", optimal_value)
+print("The path taken is:", optimal_path)
